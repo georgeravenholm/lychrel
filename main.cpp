cf58786754d0790704ae5b0e27eb40e74d656f9f
George <ravenholmgeorge@gmail.com>
2017-12-27 05:38:26 +1100

    added palendrome detections

--- main.cpp
+++ main.cpp
@@ -45,26 +45,46 @@ struct num
 		return i;
 		
 	}
+
+	bool isDromic()
+	{
+		// // check if no. of digits is even
+		//if (d.size() % 2 == 0) return false;
+
+		// lel buttpaste
+		decltype(d) first_half, second_half;
+		auto half_way = d.size() / 2;
+		std::copy(begin(d), begin(d)+half_way, back_inserter(first_half));
+		std::copy(begin(d)+half_way, end(d), back_inserter(second_half));
+
+		num h0(0); // new thing
+		num h1(0);
+		h0.d = first_half; h1.d=second_half;
+
+		h1.reverse();
+		if (d.size()%2!=0) h1.d.pop_back(); // remove middle digit if odde`
+		
+		// debug sgit
+		//std::cout << h0.toint() << ":" << h1.toint() << std::endl;
+		
+		return h0.toint()==h1.toint();
+	}
 	
 	std::vector<int> d;
 };
-
-void print_vec(const std::vector<int>& vec)
-{
-	for (auto x: vec) {
-		std::cout << ' ' << x;
-	}
-	std::cout << '\n';
-}
 
 int main()
 {
 	std::cout << "Hello, World!" << std::endl;
 
-	num teste(47);
-	print_vec(teste.d);	
+	num teste(47);	
 
 	std::cout << teste.toint() << " -> " << teste.revadd() << std::endl;
+
+	num pomf(12344321);
+	std::cout << "is " << pomf.toint() << "paenl??!";;
+
+	if (pomf.isDromic()) std::cout << "yes it is!";
 	
 	return 0;
 }
