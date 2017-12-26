9158cc3b0c9b6f09728bad1a3a27967c4a4ed777
George <ravenholmgeorge@gmail.com>
2017-12-27 05:17:55 +1100

    num class, reversing and adding

--- main.cpp
+++ main.cpp
@@ -1,14 +1,70 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm> // std::reverse
+#include <cmath> // stD::pow
 
 // class for number
-class number
+struct num
 {
+	num( int i )
+	{
+		while(i) // its fine, no reference and sgit
+		{
+			d.push_back(i%10); // push right-most digit
+			i /= 10; // cut off that digit
+		}
+
+		// we get a reverse order here, so we flip vectore
+		reverse();
+	}
+
+	void reverse()
+	{
+		std::reverse(d.begin(),d.end());
+	}
 	
+	unsigned long int revadd()
+	{
+		num rev(toint()); // copy
+
+		rev.reverse();
+
+		return toint() + rev.toint();
+	}
+
+	unsigned long int toint() const
+	{
+		unsigned long int i=0;
+		int j = 0;
+		for (auto n : d)
+		{
+			j++;
+			i += n * std::pow(10,d.size()-j);
+		}
+		return i;
+		
+	}
+	
+	std::vector<int> d;
 };
+
+void print_vec(const std::vector<int>& vec)
+{
+	for (auto x: vec) {
+		std::cout << ' ' << x;
+	}
+	std::cout << '\n';
+}
 
 int main()
 {
 	std::cout << "Hello, World!" << std::endl;
+
+	num teste(47);
+	print_vec(teste.d);	
+
+	std::cout << teste.toint() << " -> " << teste.revadd() << std::endl;
+	
 	return 0;
 }
